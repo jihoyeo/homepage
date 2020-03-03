bf43aa7884fa15e162293fa11ff3ae52f043862a
jihoyeo <jiho@kaist.ac.kr>
2020-03-03 19:47:09 +0900

    df

--- code.R
+++ code.R
@@ -5,3 +5,14 @@ new_site(theme= "gcushen/hugo-academic")
 serve_site()
 stop_server()
 remotes::install_github('rstudio/blogdown')
+
+use_git()
+use_github()
+
+
+browse_github_token(
+  scopes = c("repo", "gist", "user:email"),
+  description = "R:GITHUB_PAT",
+  host = "https://github.com"
+)
+
